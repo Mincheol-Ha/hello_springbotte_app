18fdf0dfbb102b3bb993b4710d30ffbea1b2b2f0
Mincheol-Ha <hmc0501@gmail.com>
2025-01-06 10:57:10 +0900

    Initial commit

---
+++ src/main/java/mincheol/hello_springbotte_app/HelloSpringbotteAppApplication.java
@@ -0,0 +1,13 @@
+package mincheol.hello_springbotte_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringbotteAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringbotteAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mincheol/hello_springbotte_app/HelloSpringbotteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mincheol.hello_springbotte_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringbotteAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
